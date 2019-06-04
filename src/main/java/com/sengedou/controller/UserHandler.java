28542125dee2fc35a51b2aa851d0561446f8c7dc
sen <995725622@qq.com>
2019-06-04 23:12:51 +0800

    ajax实现登录

--- src/main/java/com/sengedou/controller/UserHandler.java
+++ src/main/java/com/sengedou/controller/UserHandler.java
@@ -2,6 +2,8 @@ package com.sengedou.controller;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sengedou.bean.UserBean;
@@ -24,6 +27,7 @@ public class UserHandler {
 	private IUserService service;
 	
 	@RequestMapping("/login.do")
+	@ResponseBody
 	public ModelAndView login(HttpSession session, UserBean bean, String vercode) {
 		ModelAndView mav = new ModelAndView();
 		String realCode = (String) session.getAttribute("code");
@@ -44,6 +48,38 @@ public class UserHandler {
 
 	}
 	
+	@RequestMapping("/loginjson.do")
+	@ResponseBody
+	public Map loginJson(HttpSession session, UserBean bean, String vercode) {
+		Map<String,Object> result = new HashMap<>();
+		boolean isLoginOk = false;
+		String message = "登录失败";
+		String realCode = (String) session.getAttribute("code");
+		if (realCode!=null) {
+			if (!realCode.equalsIgnoreCase(vercode)) {
+				message = "验证码不正确，请重新输入";
+				result.put("message", message);
+				return result;
+			}
+		} else {
+			message = "验证码已过期";
+			result.put("message", message);
+			return result;
+		}
+		UserBean realBean = service.login(bean);
+		if (realBean == null) {
+			message = "用户名或密码不正确，请重新输入";
+			result.put("message", message);
+			return result;
+		} else {
+			message = "登录成功";
+			isLoginOk = true;
+			result.put("message", message);
+			result.put("isLoginOk", isLoginOk);
+			return result;
+		}
+	}
+	
 	@RequestMapping("/VlidateCode.do")
 	public void getVlidateCode(HttpSession session,HttpServletResponse response) throws IOException{
 		// 设置响应的类型格式为图片格式  
@@ -59,4 +95,5 @@ public class UserHandler {
 	    BufferedImage iamge = vlidateCode.getBuffImg();
 	    ImageIO.write(iamge, "JPEG", response.getOutputStream());
 	}
+
 }
